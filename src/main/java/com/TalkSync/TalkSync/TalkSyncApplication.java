8ec30a1644c23d89de5b36cf7c9301fa4552601d
the_codex_ <138112742+codingguru2221@users.noreply.github.com>
2025-11-09 21:17:08 +0530

    app start

---
+++ src/main/java/com/TalkSync/TalkSync/TalkSyncApplication.java
@@ -0,0 +1,13 @@
+package com.TalkSync.TalkSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TalkSync/TalkSync/TalkSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TalkSync.TalkSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
